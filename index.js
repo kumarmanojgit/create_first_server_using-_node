227b85ea78637df4eb4395c52936b0827af46415
kumarmanojgit <krmanoj72099@gmail.com>
2024-04-24 23:35:21 +0530

    mess

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server =http.createServer((req,res)=>
   res.end("This is my first server created by me !");
 });
 
-// how to identify request 
+//how to identify request 
 server.listen(9000,"127.0.0.1",()=>
 {
   console.log("listening to the port no 9000");
