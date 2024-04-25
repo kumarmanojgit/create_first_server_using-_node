4071e7e241732fd25290a0be032cffbdcfd3def1
kumarmanojgit <krmanoj72099@gmail.com>
2024-04-25 18:55:50 +0530

    update server

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const http= require("http");
 
 const server =http.createServer((req,res)=>
 {
-  res.end("This is my first server created by me !");
+  if(req.url=='/')
+  {
+    res.end("This is my first server created by me !");
+  }
+  else if(req.url=='/about')
+  {
+    res.end("this is about page");
+  }
+  else if(req.url=='/contact')
+  {
+    res.end(" this contact page ");
+  }
+  else {
+    res.writeHead(404,{"content-type":"text/html"});
+    res.end(" <h1>This is  400 error page</h1>");
+  }
 });
 
 //how to identify request 
